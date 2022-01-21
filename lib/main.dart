1b66f700186d2028996a7b012e3ce3f9d42068af
Leo Martinez <ingleo100499@hotmail.com>
2022-01-21 15:48:35 -0600

    Primera version

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(mainAPP());
+
+class mainAPP extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
